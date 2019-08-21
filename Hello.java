de1c4881059634505f082f258d6d81bdf2a2f058
victorialdp <53184730+victorialdp@users.noreply.github.com>
2019-08-21 14:34:34 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,27 @@
+// **********************************************************************************
+// Hello.java
+// **********************************************************************************
+// Program description: Print "hello"
+// Student: Victoria Lopez Del Pino
+// Date: 8/21/2019
+// Course: CMSC 255 Section 2
+// **********************************************************************************
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Victoria Lopez Del Pino");
+		System.out.println("Hello.java");
+		System.out.println("Print the word Hello");
+		System.out.println("8/21/2019");
+		System.out.println("CMSC 255 Section 2");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
+
+		
+
