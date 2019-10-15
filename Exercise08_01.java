d0964deb3e8342f237ae03a20456587c5c73bf26
victorialdp <53184730+victorialdp@users.noreply.github.com>
2019-10-14 22:23:45 -0400

    Create Exercise08_01.java

---
+++ Exercise08_01.java
@@ -0,0 +1,26 @@
+/*********************************************************************************
+(Sum elements column by column) Method  returns the sum of all the elements in
+a specified column in a matrix. Program reads a 3-by-4 matrix and displays the 
+sum of each column.                                                                        
+*********************************************************************************/ 
+import java.util.Scanner;
+
+public class Exercise08_01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter a 3-by-4 matrix row by row");
+        double[][] m = next.
+        
+        System.out.println("Sum of elements at column 0 is "+sumColumn(m, 0));
+        System.out.println("Sum of elements at column 1 is "+sumColumn(m, 1));
+        System.out.println("Sum of elements at column 2 is "+sumColumn(m, 2));
+        System.out.println("Sum of elements at column 3 is "+sumColumn(m, 3));
+        
+    }
+    
+    public static double sumColumn(double[][] m, int columnIndex) {
+        return sum.column(columnIndex);
+        
+    }
+}
